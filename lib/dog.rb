1fdaca387da1fa411e0751aea483bb5f161b639e
chic-caller-7520 <arbrown110@gmail.com>
2020-04-14 01:24:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
-# Add your code here
+class Dog 
+  @@all= []
+  attr_accessor :name
+  
+  def intialize (name)
+    @name = name
+  end
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # Add your code here
